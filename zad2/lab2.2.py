b7ebc4d5303b0dd7531ddd9942ea9601f5dbcd39
ebanat2000 <59319885+pavlovdmitrii4871@users.noreply.github.com>
2024-03-15 15:40:35 +0300

    lab2.2.py

--- zad2/lab2.2.py
+++ zad2/lab2.2.py
@@ -1,9 +1,7 @@
 from deq_stack import Dequeue
 
-
 def decrypt_message(msg, deck):
     decrypted_msg = ''
-
     for char in msg:
         while True:
             curr = deck.pop_last()
@@ -16,24 +14,18 @@ def decrypt_message(msg, deck):
                 break
     return decrypted_msg
 
-
 def main():
     filename = "ABC.txt"
     with open(filename, 'r') as file:
         encrypted_msg = file.read()
     deck = Dequeue()
-    for c in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-              'v', 'w', 'x', 'y', 'z']:
+    for c in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
         deck.push_last(c)
-
     decrypted_msg = decrypt_message(encrypted_msg, deck)
-
     print("Расшифрованное сообщение:")
     print(decrypted_msg)
-    
     with open("decrypted.txt", 'w') as file:
         file.write(decrypted_msg)
-
 
 if __name__ == "__main__":
     main()
