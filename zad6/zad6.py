f372d54f2f77b8572b6065469d2be5d56fa26aec
ebanat2000 <59319885+pavlovdmitrii4871@users.noreply.github.com>
2024-03-16 10:32:10 +0300

    zad6.py

--- zad6/zad6.py
+++ zad6/zad6.py
@@ -1,38 +1,52 @@
+# Импортирование класса Stack из файла deq_stack.py
 from deq_stack import Stack
 
+# Создание трех стеков для цифр, букв и символов
 numbers = Stack()
 chars = Stack()
 symbols = Stack()
 
+# Имя файла с символами
 file = 'symbol.txt'
-result_file = 'results.txt'  # имя файла для записи результата
+# Имя файла для записи результата
+result_file = 'results.txt'
 
+# Открытие файла в режиме чтения
 with open(file, 'r') as file:
+    # Чтение каждого символа из файла
     for c in file.read():
+        # Если символ является цифрой, добавить его в стек numbers
         if c.isdigit():
             numbers.push(c)
+        # Если символ является буквой, добавить его в стек chars
         elif c.isalpha():
             chars.push(c)
+        # Если символ не является ни цифрой, ни буквой, добавить его в стек symbols
         else:
             symbols.push(c)
 
-with open(result_file, 'w') as f:  # открываем файл для записи
+# Открытие файла в режиме записи
+with open(result_file, 'w') as f:
     f.write("Numbers:\n")
+    # Печать всех цифр из стека numbers в файл
     for _ in range(0, len(numbers)):
         num = numbers.pop()
-        f.write(num + "\n")  # записываем число в файл
-        print(num)  # выводим число в консоль
+        f.write(num + "\n")
+        print(num)
 
     f.write("\nChars:\n")
+    # Печать всех букв из стека chars в файл
     for _ in range(0, len(chars)):
         char = chars.pop()
-        f.write(char + "\n")  # записываем букву в файл
-        print(char)  # выводим букву в консоль
+        f.write(char + "\n")
+        print(char)
 
     f.write("\nSymbols:\n")
+    # Печать всех символов из стека symbols в файл
     for _ in range(0, len(symbols)):
         symbol = symbols.pop()
-        f.write(symbol + "\n")  # записываем символ в файл
-        print(symbol)  # выводим символ в консоль
+        f.write(symbol + "\n")
+        print(symbol)
 
+# Вывод сообщения после завершения программы
 print("Результат записан в файл 'results.txt'.")
