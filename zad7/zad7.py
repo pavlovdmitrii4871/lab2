7ede3786ffd611652c1cac13daae9bec35a31652
ebanat2000 <59319885+pavlovdmitrii4871@users.noreply.github.com>
2024-03-16 10:34:05 +0300

    zad7.py

--- zad7/zad7.py
+++ zad7/zad7.py
@@ -1,21 +1,33 @@
 from deq_stack import Dequeue
 
-numbers = Dequeue()
-numbers_2 = Dequeue()
+# Создаем две пустые очереди (дека)
+numbers = Dequeue()  # очередь для отрицательных чисел
+numbers_2 = Dequeue()  # очередь для положительных чисел
 
 file = 'sifri.txt'
 result_file = 'result.txt'
+
+# Открываем файл на чтение и результат на запись
 with open(file, 'r') as file, open(result_file, 'w') as result_file:
+
+    # Итерируемся по каждой строке в файле
     for c in file.readlines():
         int_c = int(c)
+
+        # Если число положительное или равно нулю, добавляем его в numbers_2
         if int_c >= 0:
             numbers_2.push_last(int_c)
+        # Если число отрицательное, добавляем его в numbers
         else:
             numbers.push_first(int_c)
+
+    # Печатаем и записываем все отрицательные числа из numbers
     for _ in range(len(numbers)):
         num = numbers.pop_last()
         result_file.write(str(num) + '\n')
         print(num)
+
+    # Печатаем и записываем все положительные числа из numbers_2
     for _ in range(len(numbers_2)):
         num = numbers_2.pop_first()
         result_file.write(str(num) + '\n')
